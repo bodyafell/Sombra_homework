4a280a990771338b0f1993a2adf6a63efb8e5a28
ser544 <serozhka123@gmail.com>
2025-11-27 21:51:18 +0200

    feat: Add Serhii

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/photo_Serhii.png"} alt="Яворницький Сергій" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Яворницький Сергій</h2>
+          <p style={styles.position}>Junior Developer</p>
         </div>
       </div>
     </main>
